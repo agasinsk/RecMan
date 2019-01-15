018ed53e0612210032f6dd860804e9521f811fc1
Artur Gasiński <arturgasinski@hotmail.com>
2019-01-15 21:35:09 +0100

    initial commit

---
+++ app/src/main/java/com/agasinsk/recman/ProfilesContract.java
@@ -0,0 +1,20 @@
+package com.agasinsk.recman;
+
+import android.provider.BaseColumns;
+
+public final class ProfilesContract {
+    // To prevent someone from accidentally instantiating the contract class,
+    // make the constructor private.
+    private ProfilesContract() {}
+
+    /* Inner class that defines the table contents */
+    public static class Profiles implements BaseColumns {
+        public static final String TABLE_NAME = "profiles";
+        public static final String COLUMN_NAME_NAME = "name";
+        public static final String COLUMN_NAME_AUDIO_FORMAT = "audioFormat";
+        public static final String COLUMN_NAME_AUDIO_DETAILS = "audioDetails";
+        public static final String COLUMN_NAME_SOURCE_FOLDER = "sourceFolder";
+        public static final String COLUMN_NAME_FILE_HANDLING = "fileHandling";
+        public static final String COLUMN_NAME_IS_DEFAULT = "isDefault";
+    }
+}
